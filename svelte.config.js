016d79bae202ecf3c53474c24392551c4490e43a
Nick Tobat <nick.tobat@kinandcarta.com>
2022-01-13 18:18:14 -0700

    svelteconfig

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 
@@ -7,17 +7,8 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build/static',
-      fallback: null
-    }),
-
-    paths: {
-      base: '/build',
-      assets: '2022-folio/static/_app',
-    },
+    paths: { base: "/" },
+    adapter: adapter(),
 
 		vite: {
 			css: {
